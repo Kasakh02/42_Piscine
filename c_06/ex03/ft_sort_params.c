43c518fff2a95ca7f067d93d45f4d37eb89de816
Henrique Correa Villa Costa De Moraes <hcorrea-@c1r4s4.42lisboa.com>
2022-08-14 19:16:07 +0100

    piscine42 new files

---
+++ c_06/ex03/ft_sort_params.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hcorrea- <hcorrea-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/14 14:07:46 by hcorrea-          #+#    #+#             */
+/*   Updated: 2022/08/14 15:13:05 by hcorrea-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	put_char(char c)
+{
+	write(1, &c, 1);
+}
+
+void	put_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		put_char(str[i]);
+		i++;
+	}
+}
+
+int	str_cmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);	
+	}
+	return (0);
+}
+
+void	str_swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	j = 1;
+	while (j < argc - 1)
+	{
+		i = 1;
+		while (i < argc - 1)
+		{
+			if (str_cmp(argv[i], argv[i + 1]) > 0)
+				str_swap(&argv[i], &argv[i + 1]);
+			i++;
+		}
+		j++;
+	}
+	i = 1;
+	while (i < argc)
+	{
+		put_str(argv[i]);
+		put_char('\n');
+		i++;
+	}
+	return (0);
+}
